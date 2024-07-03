2a35155a4bb011026702e0d9979352ff992745d5
hotaleb <169895727+hotaleb@users.noreply.github.com>
2024-07-03 18:04:50 +0200

    feat: crée la classe ReconnaissanceGestesTempsReel
    feat: crée la classe ReconnaissanceGestesTempsReel
    
    - Définit la méthode __init__ avec le chemin du modèle et le dictionnaire des labels
    - Ajoute des méthodes de base pour le chargement du modèle et la configuration de MediaPipe

--- backend/app/models/reconnaissance_gestes_temps_reel.py
+++ backend/app/models/reconnaissance_gestes_temps_reel.py
@@ -7,4 +7,13 @@ import numpy as np
 warnings.filterwarnings("ignore", category=UserWarning, module="google.protobuf.symbol_database")
 
 class ReconnaissanceGestesTempsReel:
-    pass
+    def __init__(self, chemin_modele: str, labels_dict: dict):
+        self.charger_modele(chemin_modele)
+        self.labels_dict = labels_dict
+        self.configurer_mediapipe()
+
+    def charger_modele(self, chemin_modele: str):
+        pass
+
+    def configurer_mediapipe(self):
+        pass
