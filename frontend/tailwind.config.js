87da7684c0cf3e20a73a7b6375c2bf5dc4914b7c
Saakian Sati <s.saakian@it-students.fr>
2024-07-03 16:07:19 +0200

    add:pages-header

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,24 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '320': '1728px',
+      },
+      backgroundImage: {
+        'button-gradient': 'linear-gradient(90deg, #70DD4A 0%, #43B226 100%)',
+      },
+      colors: {
+        'custom-bg': '#302C42',
+      },
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+    },
   },
   plugins: [],
-}
-
+};
