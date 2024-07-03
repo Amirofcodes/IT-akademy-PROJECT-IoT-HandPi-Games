709feeef80d6ecedd747aea2fddcf254b8d06eca
hotaleb <169895727+hotaleb@users.noreply.github.com>
2024-07-03 18:18:25 +0200

    feat: ajoute la méthode de traitement des frames
    feat: ajoute la méthode de traitement des frames
    
    - Implémente la méthode traiter_frame pour la reconnaissance des gestes en temps réel
    - Inclut la détection et le dessin des repères de la main
    - Prépare les données pour la prédiction du modèle

--- backend/app/models/reconnaissance_gestes_temps_reel.py
+++ backend/app/models/reconnaissance_gestes_temps_reel.py
@@ -22,3 +22,38 @@ class ReconnaissanceGestesTempsReel:
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_drawing_styles = mp.solutions.drawing_styles
         self.hands = self.mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+    def traiter_frame(self, frame):
+        data_aux = []
+        x_ = []
+        y_ = []
+
+        if frame is None:
+            print("Frame is None")
+            return None
+
+        frame = cv2.flip(frame, 1)
+        H, W, _ = frame.shape
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        results = self.hands.process(frame_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                self.mp_drawing.draw_landmarks(
+                    frame,
+                    hand_landmarks,
+                    self.mp_hands.HAND_CONNECTIONS,
+                    self.mp_drawing_styles.get_default_hand_landmarks_style(),
+                    self.mp_drawing_styles.get_default_hand_connections_style())
+
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    x_.append(x)
+                    y_.append(y)
+
+                for i in range(0, 21):
+                    data_aux.append(x_[i] - min(x_))
+                    data_aux.append(y_[i] - min(y_))
+
+        return frame
