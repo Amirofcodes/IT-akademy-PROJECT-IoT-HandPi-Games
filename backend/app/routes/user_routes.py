94eeb7b1ebcae29e405b647740ab0c7b111e54e4
Jaouad Bouddehbine <138374972+Amirofcodes@users.noreply.github.com>
2024-07-04 16:07:20 +0200

    Delete backend directory

--- backend/app/routes/user_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-bp = Blueprint('user', __name__, url_prefix='/api/user')
-
-@bp.route('/profile', methods=['GET'])
-def get_profile():
-    # Implement user profile retrieval logic here
-    return jsonify({'message': 'User profile'})
