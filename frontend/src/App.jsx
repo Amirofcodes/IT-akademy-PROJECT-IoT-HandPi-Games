af4bd0eff135767026e64994e524cfc1a37e66e2
glhullier <g.lhullier@it-students.fr>
2024-09-13 15:41:44 +0200

    footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/header/Header';
-import Home from './pages/Home';
-import './App.css';
-import GamePage from './pages/Game';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./pages/Home";
+import "./App.css";
+import GamePage from "./pages/Game";
 
 function App() {
   return (
     <Router>
-        <Header />
-        <main className="p-4 container mx-auto">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/game" element={<GamePage />} />
-          </Routes>
-        </main>
+      <Header />
+      <main className="p-4 container mx-auto">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<GamePage />} />
+        </Routes>
+      </main>
     </Router>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import Footer from "../components/footer/Footer";
 
 const Home = () => {
   return (
-    <div>
+    <div className="Footer">
+      <Footer />
     </div>
   );
 };
 
 export default Home;
-
